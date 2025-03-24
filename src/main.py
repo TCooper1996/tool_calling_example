8a471e1801d4fa90ae1af0e1703d2303eabb6054
Tayler Cooper <trcooper@gmail.com>
2025-03-24 12:14:23 -0400

    using pydantic models

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from langchain_anthropic import ChatAnthropic
 import dotenv
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.tools import tool
+from pydantic import BaseModel
 
 dotenv.load_dotenv()
 api_key = os.getenv("ANTHROPIC_API_KEY")
@@ -10,26 +11,39 @@ api_key = os.getenv("ANTHROPIC_API_KEY")
 secret_combination = "1234"
 safe_unlocked = False
 
-@tool
-def get_safe_combination():
+""" Tools """
+# Use the tool decorate to automatically register the function as a tool
+class GetSafeCombination(BaseModel):
     """Read the safe combination from a note."""
-    return secret_combination
 
-@tool
-def unlock_safe(combination: str) -> str:
+# When using the BaseModel class, you don't need a constructor,
+# you can just define the fields like below
+class UnlockSafe(BaseModel):
     """Unlock the safe with the given combination."""
-    if combination == secret_combination:
-        global safe_unlocked
+    combination: str
+
+""" Functions """
+# Now just a normal python function
+def unlock_safe(args: UnlockSafe) -> str:
+    global safe_unlocked
+    if args.combination == secret_combination:
         safe_unlocked = True
         return "Safe unlocked!"
     else:
-        return "Incorrect combination!"
+        return "Incorrect combination."
+    
+def get_safe_combination() -> str:
+    return secret_combination
 
-tools = [get_safe_combination, unlock_safe]
+# NOTE: This is a list of classes, not instances of classes (no parentheses)
+tools = [GetSafeCombination, UnlockSafe]
+# add tools using bind_tools
 llm = ChatAnthropic(api_key=api_key, model="claude-3-5-haiku-latest").bind_tools(tools)
+# Must always have a system message 
 instruction = SystemMessage(content="You are a helpful assistant. Use the tools provided to unlock the safe.")
 
 def main():
+    # Models have no inherent memory so you must save each message and send them all together
     messages = [HumanMessage(content="Unlock the safe.")]
     while not safe_unlocked:
         response = llm.invoke([instruction, *messages])
@@ -37,12 +51,20 @@ def main():
         # Parse the response to see what tool it chose
         tool_name = response.tool_calls[0]["name"]
         tool_args = response.tool_calls[0]["args"]
-        if tool_name == "get_safe_combination":
-            combination = get_safe_combination(tool_input=tool_args)
-            messages.append(HumanMessage(content=f"The safe combination is {combination}."))
-        elif tool_name == "unlock_safe":
-            result = unlock_safe(tool_args.values()[0])
-            messages.append(HumanMessage(content=result))
+        # look up the associated class that it selected
+        action_class = [t for t in tools if t.__name__ == tool_name][0]
+        # Create instance of the class with the arguments we got
+        action = action_class(**tool_args)
+        # Now we can use pythons BEAUTIFUL match statement to call the correct function
+        match action:
+            case GetSafeCombination():
+                combination = get_safe_combination()
+                messages.append(HumanMessage(content=f"The safe combination is {combination}."))
+            case UnlockSafe(combination=combination):
+                result = unlock_safe(combination)
+                messages.append(HumanMessage(content=result))
+            case _:
+                raise ValueError(f"Unknown action: {action}")
 
         if safe_unlocked:
             print("Safe unlocked!")
